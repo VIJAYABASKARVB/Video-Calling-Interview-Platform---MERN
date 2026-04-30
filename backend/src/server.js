b1bcaa7951ced805710c12802389eaa2a62519c5
Vijaya Baskar P <vbluben@gmail.com>
2026-04-30 07:15:59 +0530

    inngest implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path"
 import cors from "cors"
 import { fileURLToPath } from "url"
 import { connectDB } from "../src/lib/db.js";
+import { functions, inngest } from "./lib/inngest.js"
 
 const app = express();
 
@@ -12,7 +13,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 // API routes
 app.get('/madan', (req, res) => {
